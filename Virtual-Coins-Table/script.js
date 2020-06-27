dbb3de0f9b2e17f3103c7ea559cc4ec13fcc7b69
litmiz <litmiz07@gmail.com>
2020-06-27 18:35:16 +0300

    Update script.js
    fix a few mistakes

--- Virtual-Coins-Table/script.js
+++ Virtual-Coins-Table/script.js
@@ -1,8 +1,6 @@
 fetch('https://api.coincap.io/v2/assets').then(response =>
 response.json().then(data => {
-    console.log("fetched data");
     for (let i = 0; i < data.data.length; i++) {
-        console.log(`${i}`);
         document.getElementById('virtualCoinRow').innerHTML += `<tr> <td>${data.data[i].name}</td> <td>${data.data[i].symbol}</td> <td>${data.data[i].priceUsd}</td> </tr>`
     }
 }));
